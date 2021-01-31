a8a9a0c5fd5c4dfad05467bf41dbb4e3c9235afd
ollen <oli4.games@hotmail.com>
2021-01-31 17:52:56 +0100

    Updated some packages and fixed the post handeler

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 git add .
-git commit -m "3 commit"
+git commit -m "Updated some packages and fixed the post handeler"
 git branch -M master
 git remote add origin https://github.com/oliverwk/urk-farma-azure.git
 git push -u origin master
