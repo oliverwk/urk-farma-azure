7a1dc354216a43806167ba690bf2c26314423b97
ollen <30601311+oliverwk@users.noreply.github.com>
2021-02-14 15:49:14 +0100

    Added UPDATE

--- push-git.sh
+++ push-git.sh
@@ -1,6 +1,6 @@
 #git init
 git add .
-git commit -m "Added knex"
+git commit -m "Added UPDATE"
 git branch -M knex
 git remote add origin https://github.com/oliverwk/urk-farma-azure.git
 git push -u origin knex
