02a28f79387ba4cb63edd854ae03ca3bf40e1eee
ollen <30601311+oliverwk@users.noreply.github.com>
2021-02-14 15:55:48 +0100

    Added github workflows

--- push-git.sh
+++ push-git.sh
@@ -1,6 +1,6 @@
 #git init
 git add .
-git commit -m "Added UPDATE"
+git commit -m "Added github workflows"
 git branch -M knex
 git remote add origin https://github.com/oliverwk/urk-farma-azure.git
 git push -u origin knex
