bfbc7ab31c257e2a04b8d4237e4fda4b56d30b63
Ollen <30601311+oliverwk@users.noreply.github.com>
2021-02-15 09:53:39 +0100

    Added tls/ssl and the Dockerfile secrets

--- GetCert.py
+++ GetCert.py
@@ -2,12 +2,16 @@
 # -*- coding: utf-8 -*-
 import sys
 i = 1
+newlin = """
+"""
 print("Hoeveelheid argumenten:", len(sys.argv))
 if len(sys.argv) > 3:
     for file in ["cert.pem", "privkey.pem", "ca.pem"]:
+         content = sys.argv[i].replace("%20"," ").replace("|", newlin)
          file = open("./tls/"+file, "w")
-         file.writelines(sys.argv[i].replace("%20"," "))
+         file.writelines(content)
          file.close()
+         print(content)
          i += 1
 else:
    print("No files provided.")
