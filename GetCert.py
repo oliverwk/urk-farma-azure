27254d31696c1eaca6ee57a5a9713c0b229f6736
Ollen <30601311+oliverwk@users.noreply.github.com>
2021-02-15 10:02:20 +0100

    Added tls/ssl and the Dockerfile secrets

--- GetCert.py
+++ GetCert.py
@@ -4,14 +4,13 @@ import sys
 i = 1
 newlin = """
 """
-print("Hoeveelheid argumenten:", len(sys.argv))
+print("Hoeveelheid argumenten bij "+sys.argv[0]+":", len(sys.argv))
 if len(sys.argv) > 3:
     for file in ["cert.pem", "privkey.pem", "ca.pem"]:
          content = sys.argv[i].replace("%20"," ").replace("|", newlin)
          file = open("./tls/"+file, "w")
          file.writelines(content)
          file.close()
-         print(content)
          i += 1
 else:
    print("No files provided.")
--- createDockerfile.py
+++ createDockerfile.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import sys
-print(sys.argv)
+print("Hoeveelheid argumenten bij "+sys.argv[0]+":", len(sys.argv))
 if len(sys.argv) > 3:
      file = open("./Dockerfile","w")
      file.writelines("FROM node:15\n\nWORKDIR ./\n\nCOPY package.json ./\n\nRUN npm install\n\nCOPY . .\n\nENV PORT=443\nENV SQL_server=\""+sys.argv[1]+"\"\nENV SQL_Password=\""+sys.argv[2]+"\"\nENV SQL_User=\""+sys.argv[3]+"\"\n\nEXPOSE 443\n\nCMD [ \"node\", \"index.js\" ]\n")
